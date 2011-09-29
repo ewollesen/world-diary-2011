196522b96c0a24de1c1f9be94b31a59a19aa6fb1
Eric Wollesen <ericw@xmtp.net>
2011-09-28 18:49:40 -0600

    Initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wd::Application.config.secret_token = '7d086fdf5a29516bec757674a61def3f46aa879f2592b0cebbed8b2d3122d07dac2b4cdcfb8a3c183689495fa05d2cb0fd5b148006b30c29f1dac19a00386d80'
